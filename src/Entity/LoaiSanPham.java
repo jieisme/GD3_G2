63d240349f7c9cbd0ed06fa42f5e2ff0b8acbeab
iamjie <zudd4work@gmail.com>
2023-11-21 00:06:19 +0700

    2Clothes

---
+++ src/Entity/LoaiSanPham.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author zudd4
+ */
+public class LoaiSanPham {
+
+    private int id;
+    private String loai;
+
+    public LoaiSanPham() {
+    }
+
+    public LoaiSanPham(int id, String loai) {
+        this.id = id;
+        this.loai = loai;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoai() {
+        return loai;
+    }
+
+    public void setLoai(String loai) {
+        this.loai = loai;
+    }
+}
